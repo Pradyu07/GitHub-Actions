f935afe56cff3f1ed30f509660ce7928e1438357
Pradyu07 <pradyuds@inventechinfo.com>
2025-08-21 16:38:54 +0530

    Update addition.py
    added subtract

--- src/addition.py
+++ src/addition.py
@@ -6,3 +6,11 @@ def add(a, b):
 def test_add():
     assert add(1, 2) == 3
     assert add(1, -1) == 0
+    
+# this is 2nd test commit
+def subtarct(a,b):
+    return a - b
+
+def test_sub():
+    assert subtract(2, 1) == 1
+    assert subtract(1, -1) == 2
